8ed040f7adcea01e40f9fafc2a7baebc84e4587a
Jayala101 <jimmysanti1@hotmail.com>
2025-10-29 19:53:55 -0500

    hooks and users 2

--- modulo_2/react_native/mi-app-rn/App.tsx
+++ modulo_2/react_native/mi-app-rn/App.tsx
@@ -1,15 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 
-import SaludoSimple from "./src/Saludo";
-import SaludoConProps from "./src/SaludoProps";
+import SaludoSimple from "./src/components/Saludo";
+//import SaludoConProps from "./src/SaludoProps";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <SaludoSimple />
-      <SaludoConProps nombre="Francisco" />
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "#0d1117" }}>
+      <SaludoSimple/>
     </View>
   );
 }
